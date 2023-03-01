b1f684439f7f135ff5b9c671dda782cf2731fd4b
abhi-coderr <abhishek.oneclick@gmail.com>
2023-03-02 00:36:08 +0530

    fit: jay yogeshwar.(2nd march 23, 00:36)

---
+++ app/src/main/java/com/example/shatayu/ui/activity/SplashActivity.kt
@@ -0,0 +1,41 @@
+package com.example.shatayu.ui.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.example.shatayu.R
+import com.example.shatayu.databinding.ActivitySplashBinding
+
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var activitySplashBinding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(activitySplashBinding.root)
+        animationLogo()
+        navigationDelay()
+    }
+
+    private fun navigationDelay() {
+        Handler().postDelayed({
+            // Intent is used to switch from one activity to another.
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i) // invoke the SecondActivity.
+            finish() // the current activity will get finished.
+        }, 3000)
+    }
+
+    private fun animationLogo() {
+        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.logo_anim)
+        activitySplashBinding.appLogoImgView.startAnimation(slideAnimation)
+    }
+
+}
